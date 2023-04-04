0184e5d79005dac9e20abef2bba0902cd8c765d9
MariahCasciola417 <117303840+MariahCasciola@users.noreply.github.com>
2023-04-04 17:04:49 -0500

    intersect function produces the combination two sets with their ccommon values

--- src/setOperations.js
+++ src/setOperations.js
@@ -10,12 +10,21 @@ function union(s1, s2) {
  * return the intersection of two sets
  */
 function intersect(s1, s2) {
-    
+  // Write a function that accepts two Sets and return the Set containing the elements that appear in both Sets.
+  const intersection = new Set();
+  for (let element of s1) {
+    if (s2.has(element)) {
+      intersection.add(element);
+    }
+  }
+  return intersection;
 }
 
 /**
  * return the difference of two sets
  */
-function difference(s1, s2) {}
+function difference(s1, s2) {
+    
+}
 
 module.exports = { union, intersect, difference };
