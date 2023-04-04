fe30847d99a0efc7ea226cfd8afe6655772cca5d
MariahCasciola417 <117303840+MariahCasciola@users.noreply.github.com>
2023-04-04 16:52:12 -0500

    union function combines two sets into one set

--- src/setOperations.js
+++ src/setOperations.js
@@ -1,12 +1,17 @@
 /**
  * return the union of two sets
  */
-function union(s1, s2) {}
+function union(s1, s2) {
+  // Write a function that accepts two Sets and returns the union of the two Sets.
+  return new Set([...s1, ...s2]);
+}
 
 /**
  * return the intersection of two sets
  */
-function intersect(s1, s2) {}
+function intersect(s1, s2) {
+    
+}
 
 /**
  * return the difference of two sets
