0409ed90c487d76e47b4710ebf361b5604f3a911
MariahCasciola417 <117303840+MariahCasciola@users.noreply.github.com>
2023-04-04 20:53:03 -0500

    isEqual function passes all tests and checks to see if two arrays are equal

--- src/equalArrays.js
+++ src/equalArrays.js
@@ -1,6 +1,12 @@
 /**
  * return true if two arrays are equal, false otherwise
  */
-function isEqual(a1, a2) {}
+function isEqual(a1, a2) {
+  let sorteda1 = a1.sort();
+  let sorteda2 = a2.sort();
+  return sorteda1.every((element, index) => element === sorteda2[index]);
+  // returns true if a1 and a2 are equal
+  // otherwis returns false
+}
 
 module.exports = isEqual;
